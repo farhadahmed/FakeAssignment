80b074372f73d53bd16f29c41a4c762ac9b8eabf
Farhad Ahmed <farhadahmed2007@gmail.com>
2025-02-22 11:11:53 -0800

    added ability to sort products by category.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Fake assignment completed!");
+        System.out.println("New feature added!");
     }
 }
